72188019c6035f597d529540fda26e4f10d53546
SergeevRoman <49644104+SergeevRoman@users.noreply.github.com>
2023-12-09 14:05:42 +0300

    images sends only for me

--- game_helper/meta.py
+++ game_helper/meta.py
@@ -6,22 +6,21 @@ from game_helper.config_executor import bot_config_access
 TELEGRAM_TOKEN = bot_config_access('telegram_bot_token')
 YANDEX_DISK_TOKEN = bot_config_access('ya_disk_token')
 YANDEX_DISK_PUBLIC_FOLDER = bot_config_access('disk_public_folder')
+CHAT_ID = bot_config_access('chat_id')
 
 y = YaDisk(token=YANDEX_DISK_TOKEN)
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
-
-
-class Card:
-    def __init__(self, name, link):
-        self.name = name
-        self.link = link
 
 def get_files_from_yadisk(folder_path):
     files = y.listdir(folder_path)
     return [file['name'] for file in files]
 
 def send_image_from_yadisk(chat_id, image_name):
-    bot.send_photo(chat_id, image_name)
+    if chat_id == int(CHAT_ID):
+        bot.send_photo(chat_id, image_name)
+    else:
+        bot.send_message(chat_id, 'Вам нельзя получать картинки!')
+
 
 def meta(folder_path):
     meta = (y.get_meta(folder_path))
